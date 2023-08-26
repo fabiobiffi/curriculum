9ea8d84e0420ea0ccf39b65d0b7103d316eb12fe
Fabio Biffi <fabiobiffi2000@gmail.com>
2023-08-26 14:27:31 +0200

    dockerization

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
-  <!-- <Hero></Hero>
-  <Navbar></Navbar> -->
-  <RouterView />
+
+
+
+  <Hero></Hero>
 </template>
 
 <script>
@@ -9,18 +10,10 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 import "bootstrap-icons/font/bootstrap-icons.css"
 import Hero from './components/Hero.vue'
-import Navbar from './components/Navbar.vue'
 
 export default {
   components: {
     Hero,
-    Navbar
   }
 }
 </script>
-
-<style>
-html, body {
-  background: #f6f6f6 !important;
-}
-</style>
--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="hero container-fluid">
-    <div class="row h-100 profile-picture">
-      <div class="col-lg-6 d-flex justify-content-center align-items-start flex-column first-col">
-        <div class="p-5">
-            <div class="title">Fabio Biffi</div>
-            <div class="subtitle">Software Engineer</div>
-            <div class="social-icons mt-5" style="font-size: 34px;">
-                <i class="bi bi-linkedin me-3" style="color: #0a77b5;"></i>
-                <i class="bi bi-github me-3" style="color: #6cc644;"></i>
-                <i class="bi bi-instagram me-3" style="color: #E1306C"></i>
-            </div>
+  <div class="hero">
+    <section>
+      <div class="container-fluid h-100">
+        <div class="row h-100">
+          <div class="col-lg-6 d-flex align-items-center justify-content-center">
+            <div class="profile-picture"></div>
+          </div>
+          <div class="col-lg-6 d-flex justify-content-center flex-column">
+            <h1 class="title text-white">Fabio <span style="color: #FFBD59;">Biffi</span></h1>
+            <h3 class="title" style="color: #FF66C4;">Software Engineer</h3>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   </div>
 </template>
 
@@ -24,31 +24,18 @@ export default {
 
 <style lang="scss" scoped>
 .hero {
-  height: 700px;
 
+  background-color: #0f1728;
   .profile-picture {
+    width: 300px;
+    height: 300px;
+    border-radius: 1000px;
+    overflow: hidden;
     background-image: url(@/assets/profile_picture.jpeg);
-    background-position: center center;
+    background-position: center center;    
     background-size: cover;
     background-repeat: no-repeat;
-
-    .first-col {
-      background-color: rgb(81 154 145 / 90%);
-    }
-
-  };
-
-  .title {
-    color: white;
-    font-size: 50px;
-    letter-spacing: 5px;
-    text-transform: uppercase;
   }
 
-  .subtitle {
-    color: white;
-    font-size: 30px;
-    font-weight: 100;
-  }
 }
 </style>
