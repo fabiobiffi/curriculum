01162ef112898c6a17c545c22003344fd1a654ab
Fabio Biffi <info@fabiobiffi.it>
2023-09-09 12:32:26 +0200

    inspiring page

--- src/components/InspiringSoftware.vue
+++ src/components/InspiringSoftware.vue
@@ -4,58 +4,50 @@
       <div class="container-fluid h-100">
         <div class="row">
           <div class="col">
-            <h1 class="title" style="color: #A22982">
-              Inspiring<span style="color: #C9609F"> Software</span>
+            <h1 class="title" style="color: #a22982">
+              Inspiring<span style="color: #c9609f"> Software</span>
             </h1>
             <p style="color: #737373">Sep 2019 - 2022</p>
           </div>
         </div>
         <div class="row" style="height: calc(100% - 151px)">
           <div class="col-lg-6 d-flex align-items-center justify-content-center flex-column">
-            <p>
-              My role was to implement new features on the main product of the company, an high
-              traffic website (over 1 million visitors per day).
-            </p>
-
-            <p>
-              I worked on invoicing and payments module of the website, I' ve implemented new
-              payment methods and helped our finance team with a new tool for managing invoices.
-            </p>
-          </div>
-          <div class="col-lg-6">
-            <div class="container h-100">
-              <div class="row" >
-                  <div class="col">
-                    <div class="circle" style="background: #44B78B">
-                        Django
-                    </div>
-                </div>
-                  <div class="col">
-                    <div class="circle" style="background: #42D392">
-                        Vue3
-                    </div>
-                  </div>
-                  <div class="col"></div>
-              </div>
+            <div class="container h-100 p-4">
               <div class="row">
-                <div class="col"></div>
-                <div class="col"></div>
                 <div class="col">
-                    <div class="circle" style="background: #00ED64">
-                        MongoDB
-                    </div>
+                  <!-- Angular circle -->
+                  <div class="circle text-light" style="background: #DD0D31">Angular</div>
                 </div>
               </div>
               <div class="row">
                 <div class="col"></div>
                 <div class="col">
-                    <div class="circle text-white" style="background: #384d54">
-                        Docker
-                    </div>
+                  <!-- Python circle -->
+                  <div class="circle" style="background: #4584B6; color: #FFDE57;">Python</div>
                 </div>
-                <div class="col"></div>
+              </div>
+              <div class="row">
+                <div class="col">
+                  <!-- AWS circle -->
+                  <div class="circle" style="background: #FF9900">AWS</div>
+                </div>
               </div>
             </div>
+          </div>
+          <div class="col-lg-6 d-flex justify-content-center flex-column">
+            <p style="font-weight: bolder; font-size: 24px">Frontend Engineer</p>
+            <p>
+              I've worked on the main software platform made in Angular, my role was to implement
+              new applications inside the platform. <br />
+              I also have coordinated a 5 people team.
+            </p>
+            <p class="mt-4" style="font-weight: bolder; font-size: 24px">Full Stack Engineer</p>
+            <p>
+              I've worked as full stack engineer (as consultant) for an important company that
+              produces and sells energetic turbines. <br />
+              I've developed an internal tool that collects and organise data from all the turbines
+              around the world.
+            </p>
           </div>
         </div>
       </div>
@@ -71,17 +63,17 @@ export default {
 
 <style lang="scss" scoped>
 .inspiring-software {
-  background-color: #C1FF72;
+  background-color: #c1ff72;
 }
 
 .circle {
-    width: 200px;
-    height: 200px;
-    border-radius: 200px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-size: 24px;
-    font-weight: bold;
+  width: 200px;
+  height: 200px;
+  border-radius: 200px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 24px;
+  font-weight: bold;
 }
 </style>
